159e149b4dd147095680bb74785363e146b7b2d8
RNAHVI <rnahvi@hotmail.com>
2018-11-13 16:54:19 -0800

    yet other changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello from JavaScript");
+//console.log("Hello from JavaScript");
+
+
+const mapboxgl = require("mapbox-gl");
+
+//mapboxgl.accessToken = "YOUR API TOKEN HERE";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianZhc3F1ZXo4Njg2IiwiYSI6ImNqb2dmZnNiZjBlcGkzeHBoenZrZm1ob2MifQ.gj84CFB8iABE18OdqPm_0Q';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
