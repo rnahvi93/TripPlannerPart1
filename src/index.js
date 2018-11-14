1f157337e1542b6f408901a19e51af7cd87170e7
RNAHVI <rnahvi@hotmail.com>
2018-11-13 16:58:08 -0800

    more changes

--- src/index.js
+++ src/index.js
@@ -13,3 +13,10 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
